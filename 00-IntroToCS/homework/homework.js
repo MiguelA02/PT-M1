31a204e6427a8e0546ace162b378ad0ad6546174
MiguelA02 <101620100+MiguelA02@users.noreply.github.com>
2022-04-26 10:36:32 -0600

    Update homework.js

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -4,11 +4,9 @@ function BinarioADecimal(num) {
   num = num.split('').reverse();
   for(var i = 0;i < num.length;i++){
     num[i] = parseInt(num[i]);
-    num[i] = num[i] * Math.pow(2,i);  
-  }
+    num[i] = num[i] * Math.pow(2,i);}
   var decimal = num.reduce((acc,item) =>{
-  return acc = acc + item;
-  },0);
+  return acc = acc + item;},0);
   return decimal;
 
 }
@@ -18,8 +16,7 @@ function DecimalABinario(num) {
   var bin = [];
   while(num >= 1 ){
   bin.push(Math.floor(num%2));
-  num =  num/2;
-  }
+  num =  num/2;}
   bin = bin.reverse().join('');
   return bin
 }
